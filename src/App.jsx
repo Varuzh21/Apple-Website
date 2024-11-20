f607302a777ef411f8a9ad0981db2eec2f94d29f
Varuzh21 <varuzhan-grigoryan@inbox.ru>
2024-11-20 17:36:54 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
+
+const App = () => {
+    return (
+        <main className="bg-black">
+          <NavBar/>
+          <Hero />
+          <Highlights />
+        </main>
+    )
+}
+
+export default App
