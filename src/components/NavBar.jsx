2f96c76cf4ff04fc49c008f9c1f7ebd1b5f48a33
Varuzh21 <varuzhan-grigoryan@inbox.ru>
2024-11-20 21:00:11 +0400

    i implement Heron and Highlights and Video Carouserl

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
-import {appleImg, bagImg, searchImg} from "../utils/index.js";
-import {navLists} from "../constants/index.js";
+import {appleImg, bagImg, searchImg} from '../utils';
+import {navLists} from '../constants';
 
-function NavBar() {
+const Navbar = () => {
     return (
         <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-            <nav className="flex w-full scrim-max-width">
+            <nav className="flex w-full screen-max-width">
                 <img src={appleImg} alt="Apple" width={14} height={18}/>
 
                 <div className="flex flex-1 justify-center max-sm:hidden">
                     {navLists.map((nav) => (
-                        <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+                        <div key={nav}
+                             className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
                             {nav}
                         </div>
                     ))}
@@ -21,7 +22,7 @@ function NavBar() {
                 </div>
             </nav>
         </header>
-    );
+    )
 }
 
-export default NavBar;
+export default Navbar
